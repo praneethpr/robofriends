8ae7630ecad47d7645f4f05b5a173ba10aeae0e1
Praneeth Pondicherry Ravendernath <praneeth.pondicherry@tamucc.edu>
2019-12-14 18:30:20 -0600

    Updates

---
+++ precache-manifest.d2c279bd01160f8c1360a254ba6fea90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e65c4fc58ab7aef19a9335a1c098932",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "402a907179970a36b470",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "435cff76ffb79eac77b6",
+    "url": "/robofriends/static/css/main.16e61b41.chunk.css"
+  },
+  {
+    "revision": "402a907179970a36b470",
+    "url": "/robofriends/static/js/2.ee428d40.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends/static/js/2.ee428d40.chunk.js.LICENSE"
+  },
+  {
+    "revision": "435cff76ffb79eac77b6",
+    "url": "/robofriends/static/js/main.fa54c6a2.chunk.js"
+  },
+  {
+    "revision": "1e6fdd492adfb4acd267",
+    "url": "/robofriends/static/js/runtime-main.2c4b189a.js"
+  }
+]);
